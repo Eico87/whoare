04a7f9b3bef625db4946bf9242575a28d72f6da0
Eico87 <44685961+Eico87@users.noreply.github.com>
2019-06-09 20:30:09 +0200

    Update whoare.sh

--- whoare.sh
+++ whoare.sh
@@ -1,15 +1,23 @@
 #!/bin/bash
 if [ -z "$1" ]
 then
-	echo " "
-	echo "WHO ARE "
-	echo " "
-	echo "This script takes in input a txt file" 
-	echo "containing a list of ip adresses,"
-	echo "it performs a whois operation on each ip"
-	echo "and gives as output a brief summary"
-	echo "whithout any noise. Enjoy."
-	echo " "
+cat<<eof
+	 __     __   __  __   ______       ______   ______   ______
+	/\ \  _ \ \ /\ \_\ \ /\  __ \     /\  __ \ /\  == \ /\  ___\
+	\ \ \/ ".\ \\ \  __ \\ \ \/\ \    \ \  __ \\ \  __< \ \  __\
+	 \ \__/".~\_\\ \_\ \_\\ \_____\    \ \_\ \_\\ \_\ \_\\ \_____\
+	  \/_/   \/_/ \/_/\/_/ \/_____/     \/_/\/_/ \/_/ /_/ \/_____/
+                                                               
+
+				More Than whois
+	
+
+	This script takes in input a txt file containing a list of ip adresses,
+	it performs a whois operation on each ip and gives as output a brief 
+			summary whithout any noise. 
+				    Enjoy!
+eof
+
 elif [ -n "$1" ]
 then
 	for ip in $(cat $1); do
@@ -19,3 +27,9 @@ then
 		whois $ip| egrep -i "netname|orgname|organization|country|city|person|e-mail"
 	done >whoare-result
 fi	
+		
+#use egrep
+		#it would be nice to format the text like that
+#IP	orgname		country		e-mail		person		phone
+
+
